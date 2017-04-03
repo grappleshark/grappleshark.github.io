8b34428a40259a7610c2282728bb33a30cc35283
Tanja <grappleshark@gmail.com>
2017-04-03 19:05:19 +0100

    Header file, index and about

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include("head.php");
+
+?>
+
+<div class="container">
+  
+  <div class="text-center">
+
+<br><br>
+    <p><h1>Welcome to GrappleShark at GitHub</h1></p>
+	
+    <p class="lead">Add image here</p>
+	
+    
+  </div>
+  
+</div>
+  </body>
+</html>
+
+
+
